55eb7f392665cb7eab4b1165699d7db23425b5b4
Ilya Badaev <ilarious08@yandex.ru>
2023-08-09 18:02:49 +0300

    add comment_model

--- project_with_api/app_with_api/models.py
+++ project_with_api/app_with_api/models.py
@@ -10,7 +10,22 @@ class Ad(models.Model):
     )
     price = models.PositiveBigIntegerField(verbose_name='Цена')
     publish_date = models.DateField(verbose_name='Дата публикации')
+    comments = models.ManyToManyField('Comment')
 
     class Meta:
         verbose_name = 'Объявление'
         verbose_name_plural = 'Объявления'
+
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=25, verbose_name='Имя')
+    text = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name='Текст'
+    )
+    publish_date = models.DateField(verbose_name='Дата публикации')
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
